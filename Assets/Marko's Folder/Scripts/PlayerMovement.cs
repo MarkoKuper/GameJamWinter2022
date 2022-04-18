a474ebe7d2ddc61a9d5845695fc7446e21e29b72
MarkoKuper <mkuper8@gmail.com>
2022-04-18 12:47:31 -0400

    Player movement script

--- Assets/Marko's Folder/Scripts/PlayerMovement.cs
+++ Assets/Marko's Folder/Scripts/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Move Speed")]
+    public float playerSpeed;
+
+    Rigidbody myRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if(horizontalInput != 0 || verticalInput != 0)
+        {
+            Vector3 moveDirection = new Vector3(horizontalInput * playerSpeed, verticalInput * playerSpeed);
+            myRb.velocity = moveDirection;
+        }
     }
 }
