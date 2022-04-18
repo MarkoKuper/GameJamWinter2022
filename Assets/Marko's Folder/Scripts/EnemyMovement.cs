810641c4766b71a032bd25805752f2d2d4ef44e3
MarkoKuper <mkuper8@gmail.com>
2022-04-18 17:01:21 -0400

    Added Waypoints and enemy movement scripts

---
+++ Assets/Marko's Folder/Scripts/EnemyMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        
+        //target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 20);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.25f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(waypointIndex >= Waypoints.points.Length - 1)
+        {
+            EndOfPath();
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+
+    void EndOfPath()
+    {
+        //PlayerStats.instance.ReduceLives(); ;
+        //WaveSpawner.instance.enemiesAlive--;
+        Destroy(gameObject);
+    }
+}
