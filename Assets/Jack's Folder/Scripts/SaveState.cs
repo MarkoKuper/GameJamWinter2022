69b301ec132eb5f9edcc17be091c12f481fc685c
Jackeinorris <victorjv200301@gmail.com>
2022-04-20 15:26:11 -0400

    added some prefabs, bugfixing on the pause

--- Assets/Jack's Folder/Scripts/SaveState.cs
+++ Assets/Jack's Folder/Scripts/SaveState.cs
@@ -7,20 +7,11 @@ public class SaveState : MonoBehaviour
 {
     public int LevelsBeat;
     public Image[] Levels;
-    public 
-
     // Start is called before the first frame update
     void Start()
     {
         LevelsBeat = PlayerPrefs.GetInt("LevelsBeat");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LevelChecker()
     {
         for (int i = 0; i < Levels.Length; i++)
@@ -35,4 +26,5 @@ public class SaveState : MonoBehaviour
             }
         }
     }
+   
 }
